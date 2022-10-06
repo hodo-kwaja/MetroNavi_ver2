01bff48ab9db24056531c23aacb7377790f6c831
cocoiscat <demy176@naver.com>
2022-10-06 10:02:07 +0900

    feat: lowCongestPath 경로 출력

--- src/MetroNavi/MakeJson.java
+++ src/MetroNavi/MakeJson.java
@@ -91,12 +91,14 @@ public class MakeJson {
         }
     }
 
-    public static void LowTransferPath(pathInfo transPath) {
+    public static void Path(pathInfo shortPath, pathInfo transPath, pathInfo congestPath) {
         FileWriter writer = null;
         JsonObject jsonObject = new JsonObject();
         JsonArray jsonArray = new JsonArray();
+        JsonArray jsonArray1 = new JsonArray();
+        JsonArray jsonArray2 = new JsonArray();
 
-        for(SubwayData temp : transPath.path) {
+        for(SubwayData temp : shortPath.path) {
             JsonObject object = new JsonObject();
             object.addProperty("stationName", temp.stationName);
             object.addProperty("stationCode", temp.stationCode);
@@ -124,11 +126,72 @@ public class MakeJson {
 
             jsonArray.add(object);
         }
-        jsonObject.add("LowTransferPath", jsonArray);
+        jsonObject.add("ShortestPath", jsonArray);
+
+        for(SubwayData temp : transPath.path) {
+            JsonObject object = new JsonObject();
+            object.addProperty("stationName", temp.stationName);
+            object.addProperty("stationCode", temp.stationCode);
+            object.addProperty("lineId", temp.lineId);
+
+            JsonObject schedule = new JsonObject();
+            schedule.addProperty("lineDirection", temp.schedule.lineDirection);
+            schedule.addProperty("weekType", temp.schedule.weekType);
+            schedule.addProperty("hour", temp.schedule.hour);
+            schedule.addProperty("minute", temp.schedule.minute);
+            schedule.addProperty("typeName", temp.schedule.typeName);
+            schedule.addProperty("scheduleName", temp.schedule.scheduleName);
+            schedule.addProperty("congestScore", temp.schedule.congestScore);
+            schedule.addProperty("duration", temp.schedule.duration);
+            schedule.addProperty("numStep", temp.schedule.numStep);
+
+            JsonObject transfer = new JsonObject();
+            transfer.addProperty("transferNum", temp.transferNum);
+            transfer.addProperty("isTransfer", temp.transfer);
+            transfer.addProperty("transferDistance", temp.transferInfo.distance);
+            transfer.addProperty("transferTime", temp.transferInfo.timeSec);
+
+            object.add("schedule", schedule);
+            object.add("transfer", transfer);
+
+            jsonArray1.add(object);
+        }
+        jsonObject.add("LowTransferPath", jsonArray1);
+
+        for(SubwayData temp : congestPath.path) {
+            JsonObject object = new JsonObject();
+            object.addProperty("stationName", temp.stationName);
+            object.addProperty("stationCode", temp.stationCode);
+            object.addProperty("lineId", temp.lineId);
+
+            JsonObject schedule = new JsonObject();
+            schedule.addProperty("lineDirection", temp.schedule.lineDirection);
+            schedule.addProperty("weekType", temp.schedule.weekType);
+            schedule.addProperty("hour", temp.schedule.hour);
+            schedule.addProperty("minute", temp.schedule.minute);
+            schedule.addProperty("typeName", temp.schedule.typeName);
+            schedule.addProperty("scheduleName", temp.schedule.scheduleName);
+            schedule.addProperty("congestScore", temp.schedule.congestScore);
+            schedule.addProperty("duration", temp.schedule.duration);
+            schedule.addProperty("numStep", temp.schedule.numStep);
+
+            JsonObject transfer = new JsonObject();
+            transfer.addProperty("transferNum", temp.transferNum);
+            transfer.addProperty("isTransfer", temp.transfer);
+            transfer.addProperty("transferDistance", temp.transferInfo.distance);
+            transfer.addProperty("transferTime", temp.transferInfo.timeSec);
+
+            object.add("schedule", schedule);
+            object.add("transfer", transfer);
+
+            jsonArray2.add(object);
+        }
+        jsonObject.add("LowCongestPath", jsonArray2);
+
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(jsonObject);
         try {
-            writer = new FileWriter("LowTransferPath.json");
+            writer = new FileWriter("Path.json");
             writer.write(json);
         } catch (IOException e) {
             e.printStackTrace();
